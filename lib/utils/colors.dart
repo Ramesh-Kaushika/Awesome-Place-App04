c7a4c90ff277503e3a6a50079c305525045be00b
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-17 01:42:54 +0530

    create utils file

---
+++ lib/utils/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+final mainTextColor = const Color(0xff3B3636).withOpacity(0.75);
+const mainColor = Color(0xff9E00FF);
+
+const mainBlack = Colors.black;
+const mainWhite = Colors.white;
+
+const mainTile = Color(0xff8E8FFA);
+const subTile = Color(0xffC2D9FF);
+const mainCont = Color(0xffFFE500);
+
+const naturalWonders = Color(0xff2EB200);
+final naturalWondersSub = const Color(0xff2EB200).withOpacity(0.53);
+
+const nightLife = Color(0xffFF9900);
+final nightLifeSub = const Color(0xffFF9900).withOpacity(0.53);
+
+const landMarks = Color(0xff0029ff);
+final landMarksSub = const Color(0xff0029ff).withOpacity(0.53);
+
+const cultural = Color(0xffC00C00);
+final culturalSub = const Color(0xff0029ff).withOpacity(0.53);
