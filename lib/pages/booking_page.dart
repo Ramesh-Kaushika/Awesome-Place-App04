a4b00f83ed7651b41343693a2e717313f23fdeac
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-17 15:13:06 +0530

    Awesome app done all function add

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -17,6 +17,70 @@ class BookingPage extends StatelessWidget {
           ),
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              const Text(
+                "Selected Place",
+                style: TextStyle(
+                  color: mainColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //Stack
+              Stack(
+                children: [
+                  ClipRRect(
+                    child: Image.asset(
+                      "assets/Cultural.png",
+                      width: double.infinity,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.56),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(17.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Selected Place",
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w700,
+                            color: mainWhite,
+                          ),
+                        ),
+                        Text(
+                          "Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you're seeking the tranquility visit offers something for every traveler.",
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.w300,
+                            color: mainWhite,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
